4e356eb9be8b9af0c9e2f47f9061ccc5d743eb14
Hexirp <Hexirp@users.noreply.github.com>
2017-07-22 20:22:51 +0900

    Unfold Spine

--- src/Control/Monad/Zombie.hs
+++ src/Control/Monad/Zombie.hs
@@ -21,19 +21,15 @@ graftSpine c (Spine v d) = Spine v (Tree d c)
 -- | 'Zombie' is a variant of 'Skeleton' which has an 'Alternative' instance.
 data Zombie t a where
   Sunlight :: Zombie t a
-  Zombie :: Spine t (Zombie t) a -> Zombie t a -> Zombie t a
+  Zombie :: MonadView t (Zombie t) x -> Cat (Kleisli (Zombie t)) x a -> Zombie t a -> Zombie t a
 
 z_app :: Zombie t a -> Zombie t a -> Zombie t a
 z_app Sunlight ys = ys
-z_app (Zombie x xs) ys = Zombie x (z_app xs ys)
+z_app (Zombie v c xs) ys = Zombie v c (z_app xs ys)
 
-z_map :: (Spine t (Zombie t) a -> Spine s (Zombie s) b) -> Zombie t a -> Zombie s b
-z_map _ Sunlight = Sunlight
-z_map f (Zombie x xs) = Zombie (f x) (z_map f xs)
-
-zl_bind :: Zombie t a -> (Spine t (Zombie t) a -> [b]) -> [b]
-zl_bind Sunlight _ = []
-zl_bind (Zombie x xs) f = f x ++ zl_bind xs f
+zg_map :: Cat (Kleisli (Zombie t)) a b -> Zombie t a -> Zombie t b
+zg_map _ Sunlight = Sunlight
+zg_map f (Zombie v c xs) = Zombie v (Tree c f) (zg_map f xs)
 
 instance Functor (Zombie t) where
   fmap = liftM
@@ -48,8 +44,8 @@ instance Alternative (Zombie t) where
   xs <|> ys = z_app xs ys
 
 instance Monad (Zombie t) where
-  return a = Zombie (Spine (Return a) id) Sunlight
-  xs >>= k = z_map (graftSpine $ Leaf $ Kleisli k) xs
+  return a = Zombie (Return a) id Sunlight
+  xs >>= k = zg_map (Leaf $ Kleisli k) xs
 
 instance MonadPlus (Zombie t) where
   mzero = empty
@@ -62,21 +58,25 @@ liftZ t = embalm (t :>>= return)
 
 -- | Turn a decomposed form into a composed form.
 embalm :: MonadView t (Zombie t) a -> Zombie t a
-embalm t = Zombie (Spine t id) Sunlight
+embalm t = Zombie t id Sunlight
 {-# INLINE embalm #-}
 
 -- | Decompose a zombie as a list of possibilities.
 disembalm :: Zombie t a -> [MonadView t (Zombie t) a]
-disembalm ss = zl_bind ss f where
-  f (Spine v c) = case v of
-    Return a -> viewL c [Return a] $ \(Kleisli k) c' -> case k a of
-      ss' -> disembalm $ z_map (graftSpine c') ss'
-    t :>>= k -> return $ t :>>= \a -> case k a of
-      ss' -> z_map (graftSpine c) ss'
+disembalm Sunlight = []
+disembalm (Zombie v c xs) = disembalmF v c ++ disembalm xs
+
+disembalmF :: MonadView t (Zombie t) a -> Cat (Kleisli (Zombie t)) a b -> [MonadView t (Zombie t) b]
+disembalmF v c = case v of
+  Return a -> viewL c [Return a] $ \(Kleisli k) c' -> case k a of
+    ss' -> disembalm $ zg_map c' ss'
+  t :>>= k -> return $ t :>>= \a -> case k a of
+    ss' -> zg_map c ss'
 
 -- | Like 'hoistSkeleton'
 hoistZombie :: forall s t a. (forall x. s x -> t x) -> Zombie s a -> Zombie t a
 hoistZombie f = go where
   go :: forall x. Zombie s x -> Zombie t x
-  go ss = z_map (\(Spine v c) -> Spine (hoistMV f go v) (transCat (transKleisli go) c)) ss
+  go Sunlight = Sunlight
+  go (Zombie v c xs) = Zombie (hoistMV f go v) (transCat (transKleisli go) c) (go xs)
 {-# INLINE hoistZombie #-}
